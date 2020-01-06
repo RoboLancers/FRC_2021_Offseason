3938ad80304f3fd04d2e5afddb24d8811afbb473
Navin <7118018@philasd.org>
2020-01-06 18:00:33 -0500

    Create Drivetrain and incorporated constants

---
+++ src/main/java/frc/robot/Drivetrain.java
@@ -0,0 +1,39 @@
+package frc.robot;
+
+import com.revrobotics.CANSparkMax;
+import edu.wpi.first.wpilibj2.command.Subsystem;
+import frc.robot.commands.UseDrivetrain;
+import frc.robot.subsystems.TransmissionSide;
+
+public class Drivetrain implements Subsystem {
+    private Transmission left, right;
+    private static Drivetrain drivetrain;
+
+    private Drivetrain() {
+        left = new Transmission(TransmissionSide.left, Constants.LEFT_MASTER_PORT, Constants.LEFT_SLAVE_ONE, Constants.LEFT_SLAVE_TWO);
+        right = new Transmission(TransmissionSide.right, Constants.RIGHT_MASTER_PORT, Constants.LEFT_SLAVE_ONE, Constants.LEFT_SLAVE_TWO);
+        drivetrain.setDefaultCommand(new UseDrivetrain());
+    }
+    public static synchronized Drivetrain getInstance() {
+        if (drivetrain == null) {
+            drivetrain = new Drivetrain();
+        }
+        return drivetrain;
+    }
+
+    public Transmission getLeftTransmission() {
+        return left;
+    }
+
+    public Transmission getRightTransmission() {
+        return right;
+    }
+
+    public CANSparkMax getLeftMasterMotor() {
+        return left.getMaster();
+    }
+
+    public  CANSparkMax getRightMasterMotor() {
+        return right.getMaster();
+    }
+}
