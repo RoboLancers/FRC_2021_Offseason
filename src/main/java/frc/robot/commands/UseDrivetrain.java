7a03768ee72169d416b9736266990ad36ae61f08
Navin <7118018@philasd.org>
2020-01-06 18:00:56 -0500

    Create UseDrivetrain

---
+++ src/main/java/frc/robot/commands/UseDrivetrain.java
@@ -0,0 +1,39 @@
+package frc.robot.commands;
+
+import com.sun.jmx.snmp.internal.SnmpAccessControlModel;
+import edu.wpi.first.wpilibj.GenericHID;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
+import edu.wpi.first.wpilibj2.command.Subsystem;
+import frc.robot.Drivetrain;
+import frc.robot.OI;
+
+import java.util.Set;
+
+public class UseDrivetrain implements Command {
+    private Drivetrain instance;
+
+    public UseDrivetrain() {
+        this.instance = Drivetrain.getInstance();
+    }
+
+    @Override
+    public void execute() {
+        double throttle = OI.getXboxController().getY(GenericHID.Hand.kLeft);
+        double turn = OI.getXboxController().getX(GenericHID.Hand.kRight);
+
+        double leftPower = throttle - turn;
+        double rightPower = throttle + turn;
+
+        Drivetrain.getInstance().getLeftMasterMotor().set(leftPower);
+        Drivetrain.getInstance().getRightMasterMotor().set(rightPower);
+    }
+    @Override
+    public boolean isFinished() {
+        return false;
+    }
+    @Override
+    public Set<Subsystem> getRequirements() {
+        return null;
+    }
+}
