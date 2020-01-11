81f49a252d86dbe5f75a3e6371541a3874753cc5
Jenny Wen <wjenny003@gmail.com>
2020-01-11 11:01:40 -0500

    Add TargetAiming.

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -9,6 +9,7 @@ package frc.robot;
 
 import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
 import frc.robot.commands.ExampleCommand;
 import frc.robot.subsystems.ExampleSubsystem;
 import edu.wpi.first.wpilibj2.command.Command;
@@ -24,6 +25,8 @@ public class RobotContainer {
   private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
 
   private final ExampleCommand m_autoCommand = new ExampleCommand(m_exampleSubsystem);
+
+  private final InstantCommand TargetAim = new InstantCommand();
 
 
 
---
+++ src/main/java/frc/robot/commands/TargetAiming.java
@@ -0,0 +1,36 @@
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Subsystem;
+import frc.robot.Drivetrain;
+import frc.robot.subsystems.Limelight;
+
+import java.util.Set;
+
+public class TargetAiming implements Command {
+    double leftPower, rightPower, turningOffset, distanceOffset;
+    double turningkP = 0.006;
+    double distancekP = 0.02;
+    double allowedDistanceError = 20;
+
+    public TargetAiming() {
+        hasRequirement(Drivetrain.getInstance());
+    }
+
+    public void execute() {
+        if (Limelight.getInstance().hasTarget()) {
+            turningOffset = Limelight.getInstance().getXOffset();
+            distanceOffset = allowedDistanceError - Limelight.getInstance().getYOffset();
+            leftPower = (distanceOffset * distancekP) + (turningOffset * turningkP);
+            rightPower = (distanceOffset * distancekP) - (turningOffset * turningkP);
+
+            Drivetrain.getInstance().getLeftTransmission().getMaster().set(leftPower);
+            Drivetrain.getInstance().getRightTransmission().getMaster().set(rightPower);
+        }
+    }
+
+    @Override
+    public Set<Subsystem> getRequirements() {
+        return null;
+    }
+}
