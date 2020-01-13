6e4f8f6dd4ac13d0160665f69bb077ca27eb77eb
Jenny Wen <wjenny003@gmail.com>
2020-01-13 16:40:00 -0500

    Use drivetrain from RobotContainer.

--- src/main/java/frc/robot/commands/TargetAiming.java
+++ src/main/java/frc/robot/commands/TargetAiming.java
@@ -2,35 +2,38 @@ package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Subsystem;
+import frc.robot.RobotContainer;
 import frc.robot.subsystems.Drivetrain;
 import frc.robot.subsystems.Limelight;
 
 import java.util.Set;
 
 public class TargetAiming implements Command {
+    Set<Subsystem> subsystems;
+    Drivetrain drivetrain;
     double leftPower, rightPower, turningOffset, distanceOffset;
     double turningkP = 0.006;
     double distancekP = 0.02;
     double allowedDistanceError = 20;
 
-    public TargetAiming() {
-        hasRequirement(Drivetrain.getInstance());
+    public TargetAiming(Drivetrain drivetrain) {
+        this.drivetrain = drivetrain;
+        subsystems.add(RobotContainer.drivetrain);
     }
 
     public void execute() {
-        if (Limelight.getInstance().hasTarget()) {
-            turningOffset = Limelight.getInstance().getXOffset();
-            distanceOffset = allowedDistanceError - Limelight.getInstance().getYOffset();
+        if (RobotContainer.limelight.hasTarget()) {
+            turningOffset = RobotContainer.limelight.getXOffset();
+            distanceOffset = allowedDistanceError - RobotContainer.limelight.getYOffset();
             leftPower = (distanceOffset * distancekP) + (turningOffset * turningkP);
             rightPower = (distanceOffset * distancekP) - (turningOffset * turningkP);
-
-            Drivetrain.getInstance().getLeftTransmission().getMaster().set(leftPower);
-            Drivetrain.getInstance().getRightTransmission().getMaster().set(rightPower);
+            drivetrain.getLeftTransmission().getMaster().set(leftPower);
+            drivetrain.getRightTransmission().getMaster().set(rightPower);
         }
     }
 
     @Override
     public Set<Subsystem> getRequirements() {
-        return null;
+        return subsystems;
     }
 }
