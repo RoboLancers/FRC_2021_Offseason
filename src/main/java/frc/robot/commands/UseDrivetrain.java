dbd6e6c0cc1e3286147e5008ca6cb0cb02b0c4e7
Jenny Wen <wjenny003@gmail.com>
2020-01-13 16:40:26 -0500

    Add requirements.

--- src/main/java/frc/robot/commands/UseDrivetrain.java
+++ src/main/java/frc/robot/commands/UseDrivetrain.java
@@ -3,16 +3,20 @@ package frc.robot.commands;
 import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Subsystem;
+import frc.robot.RobotContainer;
 import frc.robot.subsystems.Drivetrain;
 import frc.robot.OI;
 
 import java.util.Set;
 
 public class UseDrivetrain implements Command {
+    private Set<Subsystem> subsystems;
+    private final Drivetrain drivetrain;
     double throttle, turn, leftPower, rightPower;
 
-    public UseDrivetrain() {
-        hasRequirement(Drivetrain.getInstance());
+    public UseDrivetrain(Drivetrain drivetrain) {
+        this.drivetrain = drivetrain;
+        subsystems.add(RobotContainer.drivetrain);
     }
 
     @Override
@@ -23,8 +27,8 @@ public class UseDrivetrain implements Command {
         leftPower = throttle - turn;
         rightPower = throttle + turn;
 
-        Drivetrain.getInstance().getLeftMasterMotor().set(leftPower);
-        Drivetrain.getInstance().getRightMasterMotor().set(rightPower);
+        drivetrain.getLeftMasterMotor().set(leftPower);
+        drivetrain.getRightMasterMotor().set(rightPower);
     }
     @Override
     public boolean isFinished() {
@@ -32,6 +36,6 @@ public class UseDrivetrain implements Command {
     }
     @Override
     public Set<Subsystem> getRequirements() {
-        return null;
+        return subsystems;
     }
 }
