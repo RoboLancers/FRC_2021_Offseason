45ebc080094a5a9a2a884668af4b350521dc99a5
Navin <7118018@philasd.org>
2020-01-24 16:48:32 -0500

    Made enum states for SpinnerPivot.

---
+++ src/main/java/frc/robot/enums/SpinnerPivotState.java
@@ -0,0 +1,14 @@
+package frc.robot.enums;
+
+import edu.wpi.first.wpilibj.DoubleSolenoid;
+
+public enum SpinnerPivotState {
+    UP(DoubleSolenoid.Value.kForward), DOWN(DoubleSolenoid.Value.kReverse), NEUTRAL(DoubleSolenoid.Value.kOff);
+
+    private DoubleSolenoid.Value value;
+
+    SpinnerPivotState(DoubleSolenoid.Value value){this.value = value;}
+
+    public DoubleSolenoid.Value getValue(){return value;}
+}
+
