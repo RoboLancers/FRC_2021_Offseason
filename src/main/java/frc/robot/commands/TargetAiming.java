2ee6b0f6573ab8e7e3815c6f1be14e8ae5344509
Jenny Wen <wjenny003@gmail.com>
2020-01-24 13:04:37 -0500

    Change from interfaces to abstract classes.

--- src/main/java/frc/robot/commands/TargetAiming.java
+++ src/main/java/frc/robot/commands/TargetAiming.java
@@ -1,6 +1,7 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.Subsystem;
 import frc.robot.RobotContainer;
 import frc.robot.subsystems.Drivetrain;
@@ -8,7 +9,7 @@ import frc.robot.subsystems.Drivetrain;
 import java.util.HashSet;
 import java.util.Set;
 
-public class TargetAiming implements Command {
+public class TargetAiming extends InstantCommand {
     Set<Subsystem> subsystems;
     Drivetrain drivetrain;
     double leftPower, rightPower, turningOffset, distanceOffset;
@@ -20,6 +21,7 @@ public class TargetAiming implements Command {
         subsystems = new HashSet<Subsystem>();
         this.drivetrain = drivetrain;
         subsystems.add(RobotContainer.drivetrain);
+        addRequirements(drivetrain);
     }
 
     public void execute() {
