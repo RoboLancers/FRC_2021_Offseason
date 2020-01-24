72376e3ca27431643b6e72ccbda8cc6af53386c5
Jenny Wen <wjenny003@gmail.com>
2020-01-24 13:11:19 -0500

    Add deadband to driving.

--- src/main/java/frc/robot/commands/UseDrivetrain.java
+++ src/main/java/frc/robot/commands/UseDrivetrain.java
@@ -1,7 +1,10 @@
 package frc.robot.commands;
 
+import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import edu.wpi.first.wpilibj2.command.Subsystem;
 import frc.robot.RobotContainer;
 import frc.robot.subsystems.Drivetrain;
@@ -9,21 +12,32 @@ import frc.robot.subsystems.Drivetrain;
 import java.util.HashSet;
 import java.util.Set;
 
-public class UseDrivetrain implements Command {
+public class UseDrivetrain extends CommandBase {
     Set<Subsystem> subsystems;
     private final Drivetrain drivetrain;
     double throttle, turn, leftPower, rightPower;
 
     public UseDrivetrain() {
-        subsystems = new HashSet<Subsystem>();
+        subsystems = new HashSet<>();
         drivetrain = RobotContainer.drivetrain;
-        subsystems.add(RobotContainer.drivetrain);
+        subsystems.add(drivetrain);
+        addRequirements(drivetrain);
     }
 
     @Override
     public void execute() {
-        throttle = RobotContainer.xboxController.getY(GenericHID.Hand.kLeft);
-        turn = RobotContainer.xboxController.getX(GenericHID.Hand.kRight);
+
+        if(Math.abs(RobotContainer.xboxController.getY(GenericHID.Hand.kLeft)) > 0.2) {
+            throttle = RobotContainer.xboxController.getY(GenericHID.Hand.kLeft);
+        } else {
+            throttle = 0;
+        }
+
+        if(Math.abs(RobotContainer.xboxController.getX(GenericHID.Hand.kRight)) > 0.2) {
+            turn = RobotContainer.xboxController.getX(GenericHID.Hand.kRight);
+        } else {
+            turn = 0;
+        }
 
         leftPower = throttle - turn;
         rightPower = throttle + turn;
@@ -35,6 +49,7 @@ public class UseDrivetrain implements Command {
     public boolean isFinished() {
         return false;
     }
+
     @Override
     public Set<Subsystem> getRequirements() {
         return subsystems;
