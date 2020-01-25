f34a404f457511ad0ccc047ed136b80417e230aa
Ramon <redteamftw1021@gmail.com>
2020-01-25 14:31:37 -0500

    Added the UseIntake class.

---
+++ src/main/java/frc/robot/commands/UseIntake.java
@@ -0,0 +1,27 @@
+package frc.robot.commands;
+
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import edu.wpi.first.wpilibj2.command.Subsystem;
+import frc.robot.RobotContainer;
+import frc.robot.subsystems.manipulators.intake.Intake;
+
+
+public class UseIntake extends CommandBase {
+    private final Intake intake = RobotContainer.intake;
+    public UseIntake(){
+        addRequirements(RobotContainer.intake);
+    }
+
+    @Override
+    public void execute(){
+        intake.getMotor().set(ControlMode.PercentOutput, 0.7);
+        }
+
+    @Override
+    public boolean isFinished(){
+        return false;
+    }
+
+}
