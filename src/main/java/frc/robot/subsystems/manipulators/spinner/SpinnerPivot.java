db9ba2452b72ff6f0265a726e27b9d03b6b3aa7c
shan <schen51103@gmail.com>
2020-01-25 15:09:08 -0500

    Created Spinner folder and added SpinnerPivot.

---
+++ src/main/java/frc/robot/subsystems/manipulators/spinner/SpinnerPivot.java
@@ -0,0 +1,26 @@
+package frc.robot.subsystems.manipulators.spinner;
+
+import edu.wpi.first.wpilibj.DoubleSolenoid;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
+import frc.robot.enums.SpinnerPivotState;
+
+public class SpinnerPivot extends SubsystemBase {
+
+    private DoubleSolenoid spinnerPivot;
+    private SpinnerPivotState spinnerPivotState;
+
+    public SpinnerPivot(){
+        spinnerPivot = new DoubleSolenoid(Constants.Manipulator.Spinner.SPINNER_PIVOT_PORT_UP, Constants.Manipulator.Spinner.SPINNER_PIVOT_PORT_DOWN);
+        spinnerPivotState = SpinnerPivotState.NEUTRAL;
+    }
+
+    public void setSpinnerPivot(SpinnerPivotState spinnerPivotState){
+        spinnerPivot.set(spinnerPivotState.getValue());
+    }
+
+    public SpinnerPivotState getSpinnerPivot(){
+        return spinnerPivotState;
+
+    }
+}
