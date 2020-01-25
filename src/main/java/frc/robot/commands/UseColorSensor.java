3bc9418e714c51830b344445aa1250f57a0f2252
Jenny <wjenny003@yahoo.com>
2020-01-25 13:32:08 -0500

    Deleted useless imports.

--- src/main/java/frc/robot/commands/UseColorSensor.java
+++ src/main/java/frc/robot/commands/UseColorSensor.java
@@ -1,16 +1,8 @@
 package frc.robot.commands;
 
-import com.revrobotics.ColorMatch;
-import com.revrobotics.ColorMatchResult;
-import com.revrobotics.ColorSensorV3;
-import edu.wpi.first.wpilibj.I2C;
-import edu.wpi.first.wpilibj.util.Color;
-import edu.wpi.first.wpilibj.util.ColorShim;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Subsystem;
-import frc.robot.Constants;
 import frc.robot.RobotContainer;
-import frc.robot.subsystems.ColorSensor;
 
 import java.util.HashSet;
 import java.util.Set;
