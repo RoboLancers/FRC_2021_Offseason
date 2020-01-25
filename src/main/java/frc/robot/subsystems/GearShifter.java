70ef88dcd5fc38aee99faa73232797cf0d139a8f
Jenny <wjenny003@yahoo.com>
2020-01-25 16:29:38 -0500

    Created GearShifter.

--- src/main/java/frc/robot/subsystems/GearShifter.java
+++ src/main/java/frc/robot/subsystems/GearShifter.java
@@ -1,6 +1,23 @@
 package frc.robot.subsystems;
 
+import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
 
 public class GearShifter extends SubsystemBase {
+
+    public static DoubleSolenoid gearShifter;
+
+    public GearShifter(){
+        gearShifter = new DoubleSolenoid(Constants.Manipulator.GearShifter.GEAR_SHIFTER_FAST, Constants.Manipulator.GearShifter.GEAR_SHIFTER_SLOW);
+        gearShifter.set(DoubleSolenoid.Value.kForward);
+    }
+
+    public void setHighGear(){
+        gearShifter.set(DoubleSolenoid.Value.kForward);
+    }
+
+    public void setLowGear(){
+        gearShifter.set(DoubleSolenoid.Value.kReverse);
+    }
 }
