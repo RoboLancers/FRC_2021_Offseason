516ab34f05916dd618ab28392ba7ef6054191e1f
shan <schen51103@gmail.com>
2020-01-25 15:11:16 -0500

    Removed Spinner import.

--- src/main/java/frc/robot/commands/UseSpinner.java
+++ src/main/java/frc/robot/commands/UseSpinner.java
@@ -3,7 +3,6 @@ package frc.robot.commands;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Subsystem;
 import frc.robot.RobotContainer;
-import frc.robot.subsystems.Spinner;
 
 import java.util.HashSet;
 import java.util.Set;
