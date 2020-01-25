f37252f6f1efef7ec990b01dba7664cc6f4377b8
Navin <7118018@philasd.org>
2020-01-25 10:08:34 -0500

    Created Climber

--- src/main/java/frc/robot/subsystems/Climber.java
+++ src/main/java/frc/robot/subsystems/Climber.java
@@ -1,6 +1,28 @@
 package frc.robot.subsystems;
 
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
 
 public class Climber extends SubsystemBase {
+    private CANSparkMax climber;
+
+    Climber(){
+        climber = new CANSparkMax(Constants.Manipulator.Climber.CLIMBER_PORT, CANSparkMaxLowLevel.MotorType.kBrushless);
+
+        climber.setOpenLoopRampRate(Constants.Manipulator.Climber.CLIMBER_RAMP_RATE);
+    }
+
+    public double getPosition() {
+        return climber.getEncoder().getPosition();
+    }
+
+    public void resetEncoders() {
+        climber.getEncoder().setPosition(0);
+    }
+
+    public CANSparkMax getMaster() {
+        return climber;
+    }
 }
