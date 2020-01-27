952549a74b2fb939b5efc13fc2c9c9bde4bcf0de
Jenny <wjenny003@yahoo.com>
2020-01-27 17:06:39 -0500

    Replaced old gear shifter code with new gear shifter code.

--- src/main/java/frc/robot/subsystems/GearShifter.java
+++ src/main/java/frc/robot/subsystems/GearShifter.java
@@ -3,21 +3,26 @@ package frc.robot.subsystems;
 import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
+import frc.robot.enums.GearShifterState;
+import frc.robot.enums.IntakePivotState;
 
 public class GearShifter extends SubsystemBase {
 
-    public static DoubleSolenoid gearShifter;
+    private DoubleSolenoid gearShifter;
+
+    public GearShifterState state;
 
     public GearShifter(){
-        gearShifter = new DoubleSolenoid(Constants.Manipulator.GearShifter.GEAR_SHIFTER_FAST, Constants.Manipulator.GearShifter.GEAR_SHIFTER_SLOW);
-        gearShifter.set(DoubleSolenoid.Value.kForward);
+        gearShifter = new DoubleSolenoid(Constants.Manipulator.GearShifter.GEAR_SHIFTER_HIGH, Constants.Manipulator.GearShifter.GEAR_SHIFTER_LOW);
+        state = GearShifterState.HIGHGEAR;
     }
 
-    public void setHighGear(){
-        gearShifter.set(DoubleSolenoid.Value.kForward);
+    public void setGearShifter(GearShifterState gearShifterState){
+        gearShifter.set(gearShifterState.getValue());
+        state = gearShifterState;
     }
 
-    public void setLowGear(){
-        gearShifter.set(DoubleSolenoid.Value.kReverse);
+    public GearShifterState getState() {
+        return state;
     }
 }
