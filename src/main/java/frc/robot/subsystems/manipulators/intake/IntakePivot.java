f53ab5350c339a1aace280a64a3c4920b726b58a
Jenny <wjenny003@yahoo.com>
2020-01-29 16:28:34 -0500

    Removed RobotContainer import.

--- src/main/java/frc/robot/subsystems/manipulators/intake/IntakePivot.java
+++ src/main/java/frc/robot/subsystems/manipulators/intake/IntakePivot.java
@@ -3,7 +3,6 @@ package frc.robot.subsystems.manipulators.intake;
 import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
-import frc.robot.RobotContainer;
 import frc.robot.enums.IntakePivotState;
 
 public class IntakePivot extends SubsystemBase {
