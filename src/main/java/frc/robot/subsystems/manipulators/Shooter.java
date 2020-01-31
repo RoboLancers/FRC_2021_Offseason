d203df6780f6f76f8107c1732571ce496a32e40f
Ramon <redteamftw1021@gmail.com>
2020-01-31 16:48:19 -0500

    Added loaderMotor.

--- src/main/java/frc/robot/subsystems/manipulators/Shooter.java
+++ src/main/java/frc/robot/subsystems/manipulators/Shooter.java
@@ -1,18 +1,17 @@
 package frc.robot.subsystems.manipulators;
 
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
-import com.revrobotics.CANSparkMax;
-import com.revrobotics.CANSparkMaxLowLevel;
-import edu.wpi.first.wpilibj.CAN;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 
 public class Shooter extends SubsystemBase {
-    private TalonSRX master, slave;
+    private TalonSRX master, slave, loader;
 
     public Shooter() {
         master = new TalonSRX(Constants.Manipulator.Shooter.SHOOTER_MASTER_PORT);
         slave = new TalonSRX(Constants.Manipulator.Shooter.SHOOTER_SLAVE_PORT);
+
+        loader = new TalonSRX(Constants.Manipulator.Shooter.SHOOTER_LOADER_PORT);
 
         slave.follow(master);
 
@@ -20,5 +19,9 @@ public class Shooter extends SubsystemBase {
 
     public TalonSRX getMaster() {
         return master;
+    }
+
+    public TalonSRX getLoader() {
+        return loader;
     }
 }
