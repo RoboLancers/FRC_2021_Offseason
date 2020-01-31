e18c6c7cee30358c7d214ee4bb8ad0b8a9dae1d8
Ramon <redteamftw1021@gmail.com>
2020-01-31 16:29:01 -0500

    Changed "setIntakePivot" to "set".

--- src/main/java/frc/robot/commands/ToggleIntakePivot.java
+++ src/main/java/frc/robot/commands/ToggleIntakePivot.java
@@ -2,7 +2,7 @@ package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import frc.robot.enums.IntakePivotState;
-import frc.robot.subsystems.Intake;
+
 import frc.robot.subsystems.manipulators.intake.IntakePivot;
 
 import static frc.robot.RobotContainer.intakePivot;
@@ -15,7 +15,7 @@ public class ToggleIntakePivot extends InstantCommand {
 
     @Override
     public void initialize(){
-        intakePivot.setIntakePivot(intakePivot.getState() == IntakePivotState.INTAKEUP ? IntakePivotState.INTAKEDOWN : IntakePivotState.INTAKEUP);
+        intakePivot.set(intakePivot.getState() == IntakePivotState.INTAKEUP ? IntakePivotState.INTAKEDOWN : IntakePivotState.INTAKEUP);
     }
 
 }
