604f0d220ee7dcd5cc916631b84854c3f838d1dd
Ramon <redteamftw1021@gmail.com>
2020-01-31 16:31:02 -0500

    Added a second motor to the intake.

--- src/main/java/frc/robot/subsystems/manipulators/intake/Intake.java
+++ src/main/java/frc/robot/subsystems/manipulators/intake/Intake.java
@@ -7,16 +7,20 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 
 public class Intake extends SubsystemBase {
-    private TalonSRX intake;
+    private TalonSRX intake, intake2;
 
     public Intake() {
         intake = new TalonSRX(Constants.Manipulator.Intake.INTAKE_PORT);
+        intake2 = new TalonSRX(Constants.Manipulator.Intake.INTAKE2_PORT);
+
         intake.setNeutralMode(NeutralMode.Brake);
+        intake2.setNeutralMode(NeutralMode.Brake);
     }
 
-    public TalonSRX getMotor() {
+    public TalonSRX getMainMotor() {
         return intake;
     }
+    public TalonSRX getMotor() {return intake2;}
 
 
 }
