01fbd66f0b0125a18c247aa370a3559172ea2577
Ramon <redteamftw1021@gmail.com>
2020-01-31 16:31:55 -0500

    Deleted Neo motor code.

--- src/main/java/frc/robot/subsystems/manipulators/Shooter.java
+++ src/main/java/frc/robot/subsystems/manipulators/Shooter.java
@@ -9,25 +9,16 @@ import frc.robot.Constants;
 
 public class Shooter extends SubsystemBase {
     private TalonSRX master, slave;
-    //Undecided between 775 or NEOS
-    //private CANSparkMax master, slave;
 
     public Shooter() {
         master = new TalonSRX(Constants.Manipulator.Shooter.SHOOTER_MASTER_PORT);
         slave = new TalonSRX(Constants.Manipulator.Shooter.SHOOTER_SLAVE_PORT);
 
         slave.follow(master);
-//        master = new CANSparkMax(Constants.Manipulator.Shooter.SHOOTER_MASTER_PORT, CANSparkMaxLowLevel.MotorType.kBrushless);
-//        slave = new CANSparkMax(Constants.Manipulator.Shooter.SHOOTER_SLAVE_PORT, CANSparkMaxLowLevel.MotorType.kBrushless);
 
     }
 
     public TalonSRX getMaster() {
         return master;
     }
-
-//    public CANSparkMax getMaster() {
-//        return master;
-//    }
-
 }
