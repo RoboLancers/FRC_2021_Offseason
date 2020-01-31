499e3cf3be737626f5b47e4954837d64d449dbfd
Jenny <wjenny003@yahoo.com>
2020-01-31 16:44:27 -0500

    Created Shooter and moved into own folder.

---
+++ src/main/java/frc/robot/subsystems/manipulators/shooter/Shooter.java
@@ -0,0 +1,27 @@
+package frc.robot.subsystems.manipulators.shooter;
+
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+import edu.wpi.first.wpilibj.Talon;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
+
+public class Shooter extends SubsystemBase {
+    private TalonSRX master, slave, loader;
+
+    public Shooter() {
+        master = new TalonSRX(Constants.Manipulator.Shooter.SHOOTER_MASTER_PORT);
+        slave = new TalonSRX(Constants.Manipulator.Shooter.SHOOTER_SLAVE_PORT);
+        loader = new TalonSRX(Constants.Manipulator.Shooter.SHOOTER_LOADER_PORT);
+
+        slave.follow(master);
+
+    }
+
+    public TalonSRX getMaster() {
+        return master;
+    }
+
+    public TalonSRX getLoader(){
+        return loader;
+    }
+}
