f68f53028049101c029befa17310faee7f98861d
Ramon <redteamftw1021@gmail.com>
2020-01-31 16:32:29 -0500

    deleted Color Sensor import because it was red.

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -12,7 +12,6 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import frc.robot.commands.UseColorSensor;
-import frc.robot.subsystems.ColorSensor;
 import frc.robot.subsystems.Limelight;
 
 /**
