143ffd7d831d4f2b2c80fb0ece2e27dea3846034
Navin <7118018@philasd.org>
2020-01-31 17:13:07 -0500

    Created private final to test gearShifter.

--- src/main/java/frc/robot/commands/ToggleGearShifter.java
+++ src/main/java/frc/robot/commands/ToggleGearShifter.java
@@ -1,17 +1,22 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.InstantCommand;
+import frc.robot.RobotContainer;
 import frc.robot.enums.GearShifterState;
 import frc.robot.subsystems.GearShifter;
 import static frc.robot.RobotContainer.gearShifter;
 public class ToggleGearShifter extends InstantCommand {
-
-    public ToggleGearShifter() {
+    private final GearShifter gearShifter;
+    public ToggleGearShifter(final GearShifter gearShifter) {
+        this.gearShifter = gearShifter;
         addRequirements(gearShifter);
+
     }
+
 
     @Override
     public void initialize() {
-        gearShifter.setGearShifter(gearShifter.getState() == GearShifterState.HIGHGEAR ? GearShifterState.LOWGEAR : GearShifterState.HIGHGEAR);
+        this.gearShifter.setGearShifter(gearShifter.getState() == GearShifterState.HIGHGEAR ? GearShifterState.LOWGEAR : GearShifterState.HIGHGEAR);
+
     }
 }
