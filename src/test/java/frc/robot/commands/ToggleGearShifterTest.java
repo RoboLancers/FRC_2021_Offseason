f6fd8ef73028443cda36eb88ef3c4d17ca88eaa5
Navin <7118018@philasd.org>
2020-02-01 11:29:08 -0500

    Implemented mockito method and deleted other tests.

--- src/test/java/frc/robot/commands/ToggleGearShifterTest.java
+++ src/test/java/frc/robot/commands/ToggleGearShifterTest.java
@@ -1,19 +1,33 @@
 package frc.robot.commands;
 
+
+import edu.wpi.first.wpilibj.DoubleSolenoid;
+import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import frc.robot.enums.GearShifterState;
 import frc.robot.subsystems.GearShifter;
 import org.junit.Test;
+import org.mockito.Mockito;
 
 import static org.junit.Assert.assertEquals;
 
 public class ToggleGearShifterTest {
+
     @Test
     public void testToggleMultipleTimes() {
-        final GearShifter gearShifter = new GearShifter();
+        final GearShifter gearShifter = new GearShifter(Mockito.mock(CommandScheduler.class), Mockito.mock(DoubleSolenoid.class));
+
         final ToggleGearShifter toggleGearShifter = new ToggleGearShifter(gearShifter);
         assertEquals(GearShifterState.HIGHGEAR, gearShifter.getState());
         toggleGearShifter.initialize();
         assertEquals(GearShifterState.LOWGEAR, gearShifter.getState());
-
+        toggleGearShifter.initialize();
+        assertEquals(GearShifterState.HIGHGEAR, gearShifter.getState());
+        toggleGearShifter.initialize();
+        assertEquals(GearShifterState.LOWGEAR, gearShifter.getState());
+        toggleGearShifter.initialize();
+        assertEquals(GearShifterState.HIGHGEAR, gearShifter.getState());
+        toggleGearShifter.initialize();
+        assertEquals(GearShifterState.LOWGEAR, gearShifter.getState());
     }
+
 }
