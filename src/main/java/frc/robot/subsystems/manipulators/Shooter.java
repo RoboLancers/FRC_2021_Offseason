63c5f5f83a9782fd6881f28a0e2dd53e333698e7
Ramon <redteamftw1021@gmail.com>
2020-02-03 16:15:44 -0500

    Add Encoder to Shooter.

--- src/main/java/frc/robot/subsystems/manipulators/Shooter.java
+++ src/main/java/frc/robot/subsystems/manipulators/Shooter.java
@@ -1,5 +1,6 @@
 package frc.robot.subsystems.manipulators;
 
+import com.ctre.phoenix.motorcontrol.FeedbackDevice;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
@@ -7,14 +8,17 @@ import frc.robot.Constants;
 public class Shooter extends SubsystemBase {
     private TalonSRX master, slave, loader;
 
+
+
     public Shooter() {
         master = new TalonSRX(Constants.Manipulator.Shooter.SHOOTER_MASTER_PORT);
         slave = new TalonSRX(Constants.Manipulator.Shooter.SHOOTER_SLAVE_PORT);
 
         loader = new TalonSRX(Constants.Manipulator.Shooter.SHOOTER_LOADER_PORT);
 
-        slave.follow(master);
+        master.configSelectedFeedbackSensor(FeedbackDevice.QuadEncoder);
 
+        slave.follow(master);
     }
 
     public TalonSRX getMaster() {
@@ -24,4 +28,10 @@ public class Shooter extends SubsystemBase {
     public TalonSRX getLoader() {
         return loader;
     }
+
+    public void restEncoder(){
+        master.setSelectedSensorPosition(0);
+    }
+
+
 }
