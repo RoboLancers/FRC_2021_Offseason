ec2807d678f6ce138020a1af1f307a38183eaccd
Jenny <wjenny003@yahoo.com>
2020-02-05 18:00:41 -0500

    Created ChangeShooterSpeed command.

--- src/main/java/frc/robot/subsystems/shooter/commands/ChangeShooterSpeed.java
+++ src/main/java/frc/robot/subsystems/shooter/commands/ChangeShooterSpeed.java
@@ -1,7 +1,23 @@
 package frc.robot.subsystems.shooter.commands;
 
 import edu.wpi.first.wpilibj2.command.InstantCommand;
+import frc.robot.subsystems.shooter.Shooter;
 
 public class ChangeShooterSpeed extends InstantCommand {
+
+    private final Shooter shooter;
+    private double speed;
+
+    public ChangeShooterSpeed(Shooter shooter, double speed){
+        this.shooter = shooter;
+        addRequirements(shooter);
+
+        this.speed = speed;
+    }
+
+    @Override
+    public void initialize(){
+        this.shooter.setShooterSpeed(this.speed);
+    }
 
 }
