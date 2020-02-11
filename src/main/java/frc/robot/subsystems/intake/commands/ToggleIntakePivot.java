d6eaf8d2e2dc16c5f74e5eb076d97a5db2c1244e
Jenny <wjenny003@yahoo.com>
2020-02-11 16:23:28 -0500

    Added an IR sensor for full intake.

--- src/main/java/frc/robot/subsystems/intake/commands/ToggleIntakePivot.java
+++ src/main/java/frc/robot/subsystems/intake/commands/ToggleIntakePivot.java
@@ -4,19 +4,30 @@ import edu.wpi.first.wpilibj2.command.InstantCommand;
 import frc.robot.subsystems.intake.enums.IntakePivotState;
 
 import frc.robot.subsystems.intake.IntakePivot;
+import frc.robot.subsystems.misc.IRSensor;
 
 public class ToggleIntakePivot extends InstantCommand {
 
     private final IntakePivot intakePivot;
+    private IRSensor irSensor;
 
     public ToggleIntakePivot(final IntakePivot intakePivot ){
         this.intakePivot = intakePivot;
         addRequirements(intakePivot);
+
     }
 
     @Override
     public void initialize(){
         this.intakePivot.set(intakePivot.getState() == IntakePivotState.INTAKEUP ? IntakePivotState.INTAKEDOWN : IntakePivotState.INTAKEUP);
+
+        //if all 4 ir sensors detect, pivot goes IntakePivotState,INTAKEUP
+
+        if(irSensor.isStorageFull() == true){
+            intakePivot.set(IntakePivotState.INTAKEUP);
+        } else{
+            intakePivot.set(IntakePivotState.INTAKENEUTRAL);
+        }
     }
 
 }
