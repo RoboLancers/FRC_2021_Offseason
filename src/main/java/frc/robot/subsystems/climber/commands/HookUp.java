9a166997d9ac02c4cf0c0e97808dd44d6ad514c9
Jenny <wjenny003@yahoo.com>
2020-02-11 17:44:57 -0500

    Created a hooking command for TalonSRX motor

---
+++ src/main/java/frc/robot/subsystems/climber/commands/HookUp.java
@@ -0,0 +1,26 @@
+package frc.robot.subsystems.climber.commands;
+
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.RobotContainer;
+import frc.robot.RobotMap;
+import frc.robot.subsystems.climber.Climber;
+import frc.robot.utilities.XboxController;
+
+//using 775 pro
+
+public class HookUp extends CommandBase {
+    private Climber climber;
+    private double hookPower;
+
+    public HookUp(){
+        addRequirements(RobotContainer.climber);
+    }
+
+    @Override
+    public void execute() {
+        hookPower = RobotContainer.manipulatorXboxController.getAxisValue(XboxController.Axis.LEFT_Y);
+
+        climber.getHookUpMotor().set(ControlMode.PercentOutput, hookPower);
+    }
+}
