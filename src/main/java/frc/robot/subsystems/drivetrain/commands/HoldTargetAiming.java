03b418a29b3c53e9f142733c0e66df51082748f2
Jenny Wen <wjenny003@gmail.com>
2020-02-11 17:48:19 -0500

    Add limelight to targetAiming constructor.

--- src/main/java/frc/robot/subsystems/drivetrain/commands/HoldTargetAiming.java
+++ src/main/java/frc/robot/subsystems/drivetrain/commands/HoldTargetAiming.java
@@ -4,39 +4,34 @@ import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.Subsystem;
 import frc.robot.RobotContainer;
 import frc.robot.subsystems.drivetrain.Drivetrain;
+import frc.robot.subsystems.misc.Limelight;
 
 import java.util.HashSet;
 import java.util.Set;
 
 public class HoldTargetAiming extends InstantCommand {
-    Set<Subsystem> subsystems;
     Drivetrain drivetrain;
+    Limelight limelight;
     double leftPower, rightPower, turningOffset, distanceOffset;
     double turningkP = 0.006;
     double distancekP = 0.02;
     double allowedDistanceError = 20;
 
-    public HoldTargetAiming(Drivetrain drivetrain) {
-        subsystems = new HashSet<Subsystem>();
+    public HoldTargetAiming(Drivetrain drivetrain, Limelight limelight) {
         this.drivetrain = drivetrain;
-        subsystems.add(this.drivetrain);
+        this.limelight = limelight;
         addRequirements(drivetrain);
     }
 
     @Override
     public void execute() {
-        if (RobotContainer.limelight.hasTarget()) {
-            turningOffset = RobotContainer.limelight.getXOffset();
-            distanceOffset = allowedDistanceError - RobotContainer.limelight.getYOffset();
+        if (limelight.hasTarget()) {
+            turningOffset = limelight.getXOffset();
+            distanceOffset = allowedDistanceError - limelight.getYOffset();
             leftPower = (distanceOffset * distancekP) + (turningOffset * turningkP);
             rightPower = (distanceOffset * distancekP) - (turningOffset * turningkP);
             drivetrain.getLeft().getMaster().set(leftPower);
             drivetrain.getRight().getMaster().set(rightPower);
         }
-    }
-
-    @Override
-    public Set<Subsystem> getRequirements() {
-        return subsystems;
     }
 }
