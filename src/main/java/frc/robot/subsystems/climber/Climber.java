3106b19c6de26f6568171e466a1b026a2ee27bf5
Jenny <wjenny003@yahoo.com>
2020-02-11 17:46:48 -0500

    Updated with methods for HookUp & PullUp.

--- src/main/java/frc/robot/subsystems/climber/Climber.java
+++ src/main/java/frc/robot/subsystems/climber/Climber.java
@@ -5,39 +5,38 @@ import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import frc.robot.Constants;
 import frc.robot.RobotMap;
 
 public class Climber extends SubsystemBase {
     //The Neo Motor is used to pull the hook down
     //The 775 Motor is used to lift the hook up
-    private CANSparkMax down;
-    private TalonSRX up;
+    private CANSparkMax pullUp;
+    private TalonSRX hookUp;
 
     public Climber(){
-        down = new CANSparkMax(RobotMap.Manipulator.Climber.NEO_PORT, CANSparkMaxLowLevel.MotorType.kBrushless);
-        up = new TalonSRX(RobotMap.Manipulator.Climber.TALONSRX_PORT);
+        pullUp = new CANSparkMax(RobotMap.Manipulator.Climber.NEO_PORT, CANSparkMaxLowLevel.MotorType.kBrushless);
+        hookUp = new TalonSRX(RobotMap.Manipulator.Climber.TALONSRX_PORT);
 
-        down.setOpenLoopRampRate(RobotMap.Manipulator.Climber.CLIMBER_RAMP_RATE);
+        pullUp.setOpenLoopRampRate(RobotMap.Manipulator.Climber.CLIMBER_RAMP_RATE);
     }
 
     public double getPostion() {
-        return down.getEncoder().getPosition();
+        return pullUp.getEncoder().getPosition();
     }
 
     public void resetEncoders() {
-        down.getEncoder().setPosition(0);
+        pullUp.getEncoder().setPosition(0);
     }
 
-    public CANSparkMax getDownMotor() {
-        return down;
+    public CANSparkMax getPullUpMotor() {
+        return pullUp;
     }
 
-    public TalonSRX getUpMotor(){
-        return up;
+    public TalonSRX getHookUpMotor(){
+        return hookUp;
     }
 
     public void set(int power) {
-        up.set(ControlMode.PercentOutput, power);
+        hookUp.set(ControlMode.PercentOutput, power);
     }
 }
