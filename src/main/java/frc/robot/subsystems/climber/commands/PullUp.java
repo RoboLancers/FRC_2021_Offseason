b2bdc9f5715e10921ec5615f4856001d3bb8f71a
Jenny <wjenny003@yahoo.com>
2020-02-11 17:45:47 -0500

    Created a pulling command for NEOs

---
+++ src/main/java/frc/robot/subsystems/climber/commands/PullUp.java
@@ -0,0 +1,25 @@
+package frc.robot.subsystems.climber.commands;
+
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.revrobotics.CANSparkMax;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.RobotContainer;
+import frc.robot.subsystems.climber.Climber;
+import frc.robot.utilities.XboxController;
+
+//using neo
+public class PullUp extends CommandBase {
+    private Climber climber;
+    private double pullPower;
+
+    public PullUp(){
+        addRequirements(RobotContainer.climber);
+    }
+
+    @Override
+    public void execute() {
+        pullPower = RobotContainer.manipulatorXboxController.getAxisValue(XboxController.Axis.RIGHT_Y);
+
+        climber.getPullUpMotor().set(pullPower);
+    }
+}
