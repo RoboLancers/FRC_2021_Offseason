9b8f0042d1a246a8c7e09b27d00c8d5ce4dea370
Navin <7118018@philasd.org>
2020-02-12 17:41:04 -0500

    Created class LoadNShoot to load and shoot balls once targeted.

---
+++ src/main/java/frc/robot/subsystems/shooter/commands/LoadNShoot.java
@@ -0,0 +1,23 @@
+package frc.robot.subsystems.shooter.commands;
+
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.intake.Intake;
+import frc.robot.subsystems.shooter.Shooter;
+
+public class LoadNShoot extends CommandBase {
+    private final Shooter shooter;
+    private final Intake intake;
+
+    public LoadNShoot(Shooter shooter, Intake intake){
+        this.shooter = shooter;
+        this.intake = intake;
+        addRequirements(shooter, intake);
+    }
+
+    @Override
+    public void execute(){
+        shooter.getLoaderMotor().set(ControlMode.PercentOutput, 0.7777);
+        intake.getIntakeMotor().set(ControlMode.PercentOutput, 0.7777);
+    }
+}
