614ba5141fb81e27528d120fb00a726851affc11
Navin <7118018@philasd.org>
2020-02-12 17:37:42 -0500

    Removed if statement and pidController and changed name to RevUpShooter.

--- src/main/java/frc/robot/subsystems/shooter/commands/RevUpShooter.java
+++ src/main/java/frc/robot/subsystems/shooter/commands/RevUpShooter.java
@@ -8,45 +8,28 @@ import frc.robot.subsystems.shooter.Shooter;
 
 public class RevUpShooter extends CommandBase {
     private final Shooter shooter;
-    private final Intake intake;
-    private final PIDController pidController;
     private double targetRPM;
 
-    public RevUpShooter(Shooter shooter, Intake intake, PIDController pidController, double targetRPM){
+    public RevUpShooter(Shooter shooter, double targetRPM){
         this.shooter = shooter;
-        this.intake = intake;
-        this.pidController = pidController;
         this.targetRPM = targetRPM;
-        addRequirements(shooter, intake);
+        addRequirements(shooter);
     }
 
-    //make it so PID makes targetSpeed stay on target
     @Override
     public void initialize(){
-        this.pidController.reset();
+        shooter.resetPID();
+        shooter.setTargetRPM(targetRPM);
     }
 
     @Override
     public void execute(){
-        shooter.setTargetRPM(targetRPM);
-        if(!shooter.fastEnough()){
-            shooter.setMotorToTarget();
-        } else {
-            shooter.getLoaderMotor(ControlMode.PercentOutput, 1);
-            intake.getTransferMotor().set(ControlMode.PercentOutput, 1);
-            intake.getIntakeMotor().set(ControlMode.PercentOutput, 1);
-        }
+        shooter.setMotorToTarget();
     }
-//ADD: IF FAST ENOUGH AND AIMED, THEN SHOOT
-//SEPERATE INTO DIFF COMMANDS
 
     @Override
     public boolean isFinished(){
-        return false;
+        return shooter.fastEnough();
     }
-
-
-
-
 
 }
