42461f45eab1b8eabb0b9210af4d7eb6ef0f74d2
Jenny Wen <wjenny003@gmail.com>
2020-02-15 09:41:52 -0500

    Changed Climber talonSRX port.

--- src/main/java/frc/robot/RobotMap.java
+++ src/main/java/frc/robot/RobotMap.java
@@ -16,7 +16,7 @@ public class RobotMap {
     public static final class Manipulator{
         public final class Climber {
             public static final int NEO_PORT = 7;
-            public static final int TALONSRX_PORT = 8;
+            public static final int TALONSRX_PORT = 2;
             public static final double CLIMBER_RAMP_RATE = 0.30;
         }
         public final class Spinner {
