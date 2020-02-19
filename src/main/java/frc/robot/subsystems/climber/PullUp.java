aa6ef6428d90ea414aa912a59e2fdf858215baa5
shan <schen51103@gmail.com>
2020-02-19 17:14:21 -0500

    Changed climber to only pullup

--- src/main/java/frc/robot/subsystems/climber/PullUp.java
+++ src/main/java/frc/robot/subsystems/climber/PullUp.java
@@ -7,15 +7,13 @@ import com.revrobotics.CANSparkMaxLowLevel;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.RobotMap;
 
-public class Climber extends SubsystemBase {
+public class PullUp extends SubsystemBase {
     //The Neo Motor is used to pull the hook down
-    //The 775 Motor is used to lift the hook up
+    //The talonSRX Motor is used to lift the hook up
     private CANSparkMax pullUp;
-    private TalonSRX hookUp;
 
-    public Climber(){
+    public PullUp(){
         pullUp = new CANSparkMax(RobotMap.Manipulator.Climber.NEO_PORT, CANSparkMaxLowLevel.MotorType.kBrushless);
-        hookUp = new TalonSRX(RobotMap.Manipulator.Climber.TALONSRX_PORT);
         pullUp.restoreFactoryDefaults();
         pullUp.setInverted(true);
         pullUp.setOpenLoopRampRate(RobotMap.Manipulator.Climber.CLIMBER_RAMP_RATE);
@@ -33,11 +31,7 @@ public class Climber extends SubsystemBase {
         return pullUp;
     }
 
-    public TalonSRX getHookUpMotor(){
-        return hookUp;
-    }
-
     public void set(int power) {
-        hookUp.set(ControlMode.PercentOutput, power);
+        pullUp.set(power);
     }
 }
