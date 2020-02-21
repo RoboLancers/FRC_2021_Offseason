3d5795b9c26005a9b5496a6c22288b2479318116
Navin <7118018@philasd.org>
2020-02-21 16:54:53 -0500

    Renamed Sensors

--- src/main/java/frc/robot/subsystems/misc/IRSensor.java
+++ src/main/java/frc/robot/subsystems/misc/IRSensor.java
@@ -4,37 +4,37 @@ import edu.wpi.first.wpilibj.DigitalInput;
 import frc.robot.RobotMap;
 
 public class IRSensor {
-    private DigitalInput IR2, IR4, IR3, IR1;
+    private DigitalInput IRTwo, IRFour, IRThree, IROne;
 
     public IRSensor(){
+        IROne = new DigitalInput(RobotMap.Sensors.IR.FIRST_IR_PORT);
+        IRTwo = new DigitalInput(RobotMap.Sensors.IR.SECOND_IR_PORT);
+        IRThree = new DigitalInput(RobotMap.Sensors.IR.THIRD_IR_PORT);
+        IRFour = new DigitalInput(RobotMap.Sensors.IR.FOURTH_IR_PORT);
 
-        IR2 = new DigitalInput(RobotMap.Sensors.IR.SECOND_IR_PORT);
-        IR4 = new DigitalInput(RobotMap.Sensors.IR.FOURTH_IR_PORT);
-        IR3 = new DigitalInput(RobotMap.Sensors.IR.THIRD_IR_PORT);
-        IR1 = new DigitalInput(RobotMap.Sensors.IR.FIRST_IR_PORT);
     }
 
-    public boolean getIR2(){
-        return IR2.get();
+    public boolean getIRTwo(){
+        return IRTwo.get();
     }
 
-    public boolean getIR4(){
-        return IR4.get();
+    public boolean getIRFour(){
+        return IRFour.get();
     }
 
-    public boolean getIR3(){
-        return IR3.get();
+    public boolean getIRThree(){
+        return IRThree.get();
     }
 
-    public boolean getIR1(){
-        return IR1.get();
+    public boolean getIROne(){
+        return IROne.get();
     }
 
     public boolean isStorageFull(){
-        return IR2.get() && IR3.get() && IR4.get() && IR1.get();
+        return IRTwo.get() && IRThree.get() && IRFour.get() && IROne.get();
 
     }
     public boolean isThreeBallsIn(){
-        return IR2.get() && IR3.get();
+        return IRTwo.get() && IRThree.get();
     }
 }
