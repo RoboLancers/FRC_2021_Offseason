71936d86b8d8b6bab7943687188f3d5aa6aab7fd
Ramon <redteamftw1021@gmail.com>
2020-02-21 17:44:56 -0500

    Created UseLoaderMotor.

---
+++ src/main/java/frc/robot/subsystems/shooter/commands/UseLoaderMotor.java
@@ -0,0 +1,25 @@
+package frc.robot.subsystems.shooter.commands;
+
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.shooter.Loader;
+
+public class UseLoaderMotor extends CommandBase {
+    private Loader loader;
+    private double loaderMotorPower;
+
+    public UseLoaderMotor(Loader loader, double loaderMotorPower) {
+        this.loader = loader;
+        this.loaderMotorPower = loaderMotorPower;
+        addRequirements(loader);
+    }
+
+    @Override
+    public void execute() {
+        loader.getLoaderMotor().set(ControlMode.PercentOutput, 0.7);
+    }
+    @Override
+    public void end(boolean interrupted) {
+        loader.getLoaderMotor().set(ControlMode.PercentOutput, 0);
+    }
+}
