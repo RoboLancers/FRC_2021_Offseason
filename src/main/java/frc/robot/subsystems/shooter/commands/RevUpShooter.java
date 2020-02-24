5c07adef76d4e0d65d30b1eb6babc9115e63c4e8
shan <schen51103@gmail.com>
2020-02-24 16:45:53 -0500

    Changed methods for NEO relevancy.

--- src/main/java/frc/robot/subsystems/shooter/commands/RevUpShooter.java
+++ src/main/java/frc/robot/subsystems/shooter/commands/RevUpShooter.java
@@ -1,6 +1,5 @@
 package frc.robot.subsystems.shooter.commands;
 
-import com.ctre.phoenix.motorcontrol.ControlMode;
 import edu.wpi.first.wpilibj.controller.PIDController;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.Constants;
