9eb85911502f2862d7abcccd2edb6793615b5871
Ramon <redteamftw1021@gmail.com>
2020-02-24 16:13:25 -0500

    Added two cameras, one in the front, and one in the back.

---
+++ src/main/java/frc/robot/subsystems/misc/Camera.java
@@ -0,0 +1,30 @@
+package frc.robot.subsystems.misc;
+
+import edu.wpi.cscore.UsbCamera;
+import edu.wpi.first.cameraserver.CameraServer;
+
+public class Camera {
+    private static Camera camera;
+    private UsbCamera frontLivecam, backLivecam;
+    public Camera(){
+        frontLivecam = CameraServer.getInstance().startAutomaticCapture();
+        frontLivecam.setResolution(320, 240);
+        frontLivecam.setFPS(15);
+        backLivecam = CameraServer.getInstance().startAutomaticCapture();
+        backLivecam.setResolution(240, 180);
+        backLivecam.setFPS(15);
+
+    }
+
+
+    public UsbCamera getFrontCamera(){
+        return frontLivecam;
+    }
+
+    public UsbCamera getBackCamera(){
+        return backLivecam;
+    }
+
+
+
+}
