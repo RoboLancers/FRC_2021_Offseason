28f5c4b1e07f753402b677ba316f31775cd86daf
shan <schen51103@gmail.com>
2020-02-24 17:47:39 -0500

    Finished UseSpinner command.

--- src/main/java/frc/robot/subsystems/spinner/commands/UseSpinner.java
+++ src/main/java/frc/robot/subsystems/spinner/commands/UseSpinner.java
@@ -1,21 +1,26 @@
 package frc.robot.subsystems.spinner.commands;
 
+import com.ctre.phoenix.motorcontrol.ControlMode;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.spinner.Spinner;
 
-/**UNFNISHED CODE*/
 public class UseSpinner extends CommandBase {
     private Spinner spinner;
     private double spinnerPower;
 
     public UseSpinner(Spinner spinner, double spinnerPower) {
-        addRequirements(spinner);
         this.spinnerPower = spinnerPower;
+        addRequirements(spinner);
         this.spinner = spinner;
     }
 
     @Override
     public void execute() {
-        spinner.set(spinnerPower);
+        spinner.getSpinner().set(ControlMode.PercentOutput, 1);
+    }
+
+    @Override
+    public void end(boolean interrupted) {
+        spinner.getSpinner().set(ControlMode.PercentOutput, 0);
     }
 }
