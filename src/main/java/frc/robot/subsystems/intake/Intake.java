898b8d9b7d587f1a6503fc5cf89ee075e83e7cc8
Navin <7118018@philasd.org>
2020-02-24 16:16:28 -0500

    Removed useless imports.

--- src/main/java/frc/robot/subsystems/intake/Intake.java
+++ src/main/java/frc/robot/subsystems/intake/Intake.java
@@ -1,13 +1,10 @@
 package frc.robot.subsystems.intake;
 
-import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.NeutralMode;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import frc.robot.Constants;
 import frc.robot.RobotMap;
 
-/**MISSING CONSTANTS*/
 public class Intake extends SubsystemBase {
     private TalonSRX intake, transfer;
 
