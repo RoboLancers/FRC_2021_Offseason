5a71bbbd4b077bd54165a21a32344169d8cc3871
Calvin Macatantan <calmacatantan@gmail.com>
2021-10-13 20:55:50 -0400

    Changed Ports

--- src/main/java/frc/robot/RobotMap.java
+++ src/main/java/frc/robot/RobotMap.java
@@ -26,14 +26,14 @@ public class RobotMap {
         }
         public static final class Intake {
             public static final int INTAKE_PORT = 1;
-            public static final int INTAKE2_PORT = 3;
+            public static final int TRANSFER_PORT = 3;
             public static final int INTAKE_PIVOT_PORT_UP = 4;
             public static final int INTAKE_PIVOT_PORT_DOWN = 5;
         }
         public static final class Shooter {
             public static final int SHOOTER_MAIN_PORT = 7;
             public static final int SHOOTER_SECONDARY_PORT = 8;
-            public static final int SHOOTER_LOADER_PORT = 4;
+            public static final int SHOOTER_LOADER_PORT = 2;
         }
         public static final class GearShifter {
             public static final int GEAR_SHIFTER_HIGH = 2;
