6c9ca48dfd60bb97425ffcf50c3b26beaa539eb6
Phong Nguyen <calmacatantan@gmail.com>
2021-10-15 16:40:30 -0400

    Revert "Changed power values for intake"
    This reverts commit 32741f5662adddb0d758f82b327fb16ad61fc09c.

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -4,7 +4,6 @@ import edu.wpi.first.networktables.NetworkTableEntry;
 import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.wpilibj.geometry.Pose2d;
 import edu.wpi.first.wpilibj.geometry.Rotation2d;
-import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj.util.Units;
 import edu.wpi.first.wpilibj2.command.Command;
@@ -37,7 +36,6 @@ import frc.robot.subsystems.spinner.Spinner;
 import frc.robot.subsystems.spinner.SpinnerPivot;
 import frc.robot.utilities.FlightController;
 import frc.robot.utilities.XboxController;
-import frc.robot.subsystems.misc.Camera;
 
 public class RobotContainer {
     // Robot subsystems and commands
@@ -66,8 +64,6 @@ public class RobotContainer {
 
     public Autonomous autonomous;
 
-    public SendableChooser autoChooser;
-
     public static XboxController driverXboxController = new XboxController(0, 0.2);
     public static XboxController manipulatorXboxController = new XboxController(1, 0.2);
     public static XboxController testController = new XboxController(2,0.2);
@@ -75,7 +71,6 @@ public class RobotContainer {
     public RobotContainer() {
         networkInterface = new NetworkInterface();
         camera = new Camera();
-        autoChooser = new SendableChooser<>();
 
         // SmartDashboard.putStr
         // ^ ?
@@ -129,7 +124,7 @@ public class RobotContainer {
                 // B Held           ->      LoadNShoot()
                 // Y Held           ->      HoldTargetAiming(aimingTarget: AimingTarget.LINE)
                 // X Held           ->      UseLoaderMotor(loaderMotorPower: 0.6)
-                .whileHeld(XboxController.Button.A, new ToggleGearShifter(gearShifter))
+                .whileHeld(XboxController.Button.A, new HoldTargetAiming(drivetrain, limelight, AimingTarget.TRENCH))
                 .whileHeld(XboxController.Button.Y, new HoldTargetAiming(drivetrain, limelight, AimingTarget.LINE))
 
 
@@ -137,7 +132,7 @@ public class RobotContainer {
                 // Left Pressed     ->      ToggleGearShift()
                 // Right Held       ->      UseIntake(intakeMotorPower: 0.6, transferMotorPower: 0.6)
                 .whenPressed(XboxController.Button.LEFT_BUMPER, new ToggleIntakePivot(intakePivot))
-                .whileHeld(XboxController.Button.RIGHT_BUMPER, new UseIntake(intake, irsensor, 0.5, 0))
+                .whileHeld(XboxController.Button.RIGHT_BUMPER, new UseIntake(intake, irsensor, -0.8, 0))
 
 
             // Triggers
@@ -145,7 +140,7 @@ public class RobotContainer {
                 // Left Released    ->      RevUpShooter(targetRPM: 0)
                 // Right Pressed    ->      
                 // Right Released   ->      RevUpShooter(targetRPM: 0)
-                .whileHeld(XboxController.Trigger.RIGHT_TRIGGER, new UseIntake(intake, irsensor, -0.5, 0));
+                .whileHeld(XboxController.Trigger.RIGHT_TRIGGER, new UseIntake(intake, irsensor,0.6, 0));
 
                 //.whenPressed(XboxController.Trigger.RIGHT_TRIGGER, new UseIntake(intake, irsensor, 0.8, 0))
                 //.whenReleased(XboxController.Trigger.RIGHT_TRIGGER, new UseIntake(intake, irsensor, 0, 0));
@@ -156,13 +151,13 @@ public class RobotContainer {
                 // B Held           ->      UseIntake(intakeMotorPower: 0, transferMotorPower: -0.6)
                 // X Held           ->      UseIntake(intakeMotorPower: 1, transferMotorPower: 0)
                 // Y Held           ->      UseIntake(intakeMotorPower: 0, transferMotorPower: 0.6)
-                .whileHeld(XboxController.Button.A, new UseIntake(intake, irsensor,-0.5, 0))
+                .whileHeld(XboxController.Button.A, new UseIntake(intake, irsensor,-0.6, 0))
                     //transfers power cells out of robot
-                .whileHeld(XboxController.Button.B, new UseIntake(intake, irsensor,0, -0.4))
+                .whileHeld(XboxController.Button.B, new UseIntake(intake, irsensor,0, -0.6))
                     //takes power cells out of robot
-                .whileHeld(XboxController.Button.X, new UseIntake(intake, irsensor,0.5, 0))
+                .whileHeld(XboxController.Button.X, new UseIntake(intake, irsensor,0.6, 0))
                     //takes power cells into robot
-                .whileHeld(XboxController.Button.Y, new UseIntake(intake, irsensor, 0.5, 0.4))
+                .whileHeld(XboxController.Button.Y, new UseIntake(intake, irsensor, 0, 0.6))
                     //transfers power cells into robot
 
 
@@ -215,8 +210,5 @@ public class RobotContainer {
         SmartDashboard.putNumber("Timer", loader.getTimer().get());
         SmartDashboard.putNumber("Shooter Current 1", shooter.getMaster().getOutputCurrent());
         SmartDashboard.putNumber("Shooter Current 2", shooter.getSlave().getOutputCurrent());
-        
-        SmartDashboard.putData("Autonomous", autoChooser);
-        
     }
 }
