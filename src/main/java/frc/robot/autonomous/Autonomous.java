22e4337142d4b5ff2c07af39aeb4b180df6fc985
Phong Nguyen <calmacatantan@gmail.com>
2021-10-25 17:36:44 -0400

    auto 3

--- src/main/java/frc/robot/autonomous/Autonomous.java
+++ src/main/java/frc/robot/autonomous/Autonomous.java
@@ -37,6 +37,8 @@ public class Autonomous {
                 robotContainer.networkInterface.getCurrentlySelectedAutonomousEntry().setString(autonomousCommand.getName());
             }
         }
+
+            
         previouslySelectedObjective = selectedObjective;
         previouslySelectedStartingPosition = selectedStartingPosition;
 
