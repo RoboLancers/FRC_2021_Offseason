aee76d837d1de67014a453d3e87174af0c026de8
Calvin Macatantan <calmacatantan@gmail.com>
2021-10-29 07:41:52 -0400

    added rate limiter to drivetrain

--- src/main/java/frc/robot/subsystems/drivetrain/commands/UseDrivetrain.java
+++ src/main/java/frc/robot/subsystems/drivetrain/commands/UseDrivetrain.java
@@ -4,23 +4,31 @@ import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.drivetrain.Drivetrain;
 import frc.robot.utilities.Utilities;
 import frc.robot.utilities.XboxController;
+import edu.wpi.first.wpilibj.SlewRateLimiter;
+
 
 public class UseDrivetrain extends CommandBase {
     private final Drivetrain drivetrain;
     private final XboxController xboxController;
+    public SlewRateLimiter filter1;
+    public SlewRateLimiter filter2;
+
     double throttle, turn, leftPower, rightPower;
 
     public UseDrivetrain(Drivetrain subsystem, XboxController xboxController) {
         this.drivetrain = subsystem;
         this.xboxController = xboxController;
         addRequirements(drivetrain);
+        filter1 = new SlewRateLimiter(0.8);
+        filter2 = new SlewRateLimiter(0.5);
     }
 
     @Override
     public void execute() {
         
-        throttle = xboxController.getAxisValue(XboxController.Axis.LEFT_Y);
-        turn = xboxController.getAxisValue(XboxController.Axis.RIGHT_X);
+
+        throttle = filter1.calculate(xboxController.getAxisValue(XboxController.Axis.LEFT_Y))*0.5;
+        turn = (xboxController.getAxisValue(XboxController.Axis.RIGHT_X))*0.5;
 
 
         leftPower = throttle + turn;
@@ -32,8 +40,8 @@ public class UseDrivetrain extends CommandBase {
         rightPower = -xboxController.getAxisValue(XboxController.Axis.RIGHT_Y);
         */
 
-        drivetrain.getLeft().getMain().set(Math.pow(leftPower,3));
-        drivetrain.getRight().getMain().set(Math.pow(rightPower,3));
+        drivetrain.getLeft().getMain().set(leftPower);
+        drivetrain.getRight().getMain().set(rightPower);
     }
 
     @Override
