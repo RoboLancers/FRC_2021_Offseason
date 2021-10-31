af6e37364b2e148bc96bd064eb317c0f5d877665
Calvin Macatantan <calmacatantan@gmail.com>
2021-10-31 19:11:24 -0400

    added new transfer command for shootball auto

--- src/main/java/frc/robot/subsystems/shooter/commands/NewShoot.java
+++ src/main/java/frc/robot/subsystems/shooter/commands/NewShoot.java
@@ -1,44 +1,36 @@
-package frc.robot.subsystems.shooter.commands;
+ package frc.robot.subsystems.shooter.commands;
 
-import com.ctre.phoenix.motorcontrol.ControlMode;
-import edu.wpi.first.wpilibj.Timer;
-import edu.wpi.first.wpilibj2.command.CommandBase;
-import edu.wpi.first.wpilibj2.command.WaitCommand;
-import frc.robot.subsystems.intake.Intake;
-import frc.robot.subsystems.misc.IRSensor;
-import frc.robot.subsystems.shooter.Loader;
-import frc.robot.subsystems.shooter.Shooter;
+ import com.revrobotics.ControlType;
+ import com.ctre.phoenix.motorcontrol.ControlMode;
+ import edu.wpi.first.wpilibj.Timer;
+ import edu.wpi.first.wpilibj2.command.CommandBase;
+ import edu.wpi.first.wpilibj2.command.WaitCommand;
+ import frc.robot.subsystems.intake.Intake;
+ import frc.robot.subsystems.misc.IRSensor;
+ import frc.robot.subsystems.shooter.Loader;
+ import frc.robot.subsystems.shooter.Shooter;
 
-public class NewShoot extends Commandbase {
-    private final Loader loader;
-    private final Intake intake;
-    private IRSensor irSensor;
+ public class NewShoot extends CommandBase {
+     private final Loader loader;
+     private final Intake intake;
+     double power;
 
-    public NewShoot (Loader loader, Intake intake, IRSensor irSensor) {
+     public NewShoot (Loader loader, Intake intake, double power) {
         this.loader = loader;
         this.intake = intake;
-        this.irSensor = irSensor;
-
+        this.power = power;
         addRequirements(loader, intake);
-    }
+     }
 
-    @Override
-    public void execute() {
-        loader.getLoaderMotor().set(ControlMode.PercentOutput, 0.6);
-        intake.getIntakeMotor().set(ControlMode.PercentOutput, 0.3);
-        intake.getTransferMotor().set(ControlMode.PercentOutput, 0.3);
-    }
+     @Override
+     public void execute() {
+         loader.getLoaderMotor().set(ControlMode.PercentOutput, power);
+         intake.getIntakeMotor().set(ControlMode.PercentOutput, power/2);
+         intake.getTransferMotor().set(ControlMode.PercentOutput, power/2);
+     }
 
-    @Override
-    public void end() {
-        loader.getLoaderMotor().set(ControlMode.PercentOutput, 0);
-        intake.getIntakeMotor().set(ControlMode.PercentOutput, 0);
-        intake.getTransferMotor().set(ControlMode.PercentOutput, 0);
-    }
-
-    @Override
-    public boolean isFinished() {
-        return true;
-    }
-
-} 
+     @Override
+     public boolean isFinished() {
+         return true;
+     }
+ } 
