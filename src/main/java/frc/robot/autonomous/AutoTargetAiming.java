3567b7c3179793c2c4fced90cb842cb6b59e123d
mattperls-code <mattperls.code@gmail.com>
2021-11-15 16:45:50 -0500

    v1.0.00 ~ Initial Commit ~ Autonomous is built but untested ~ Pipeline needs to be built

--- src/main/java/frc/robot/autonomous/AutoTargetAiming.java
+++ src/main/java/frc/robot/autonomous/AutoTargetAiming.java
@@ -9,11 +9,6 @@ public class AutoTargetAiming extends CommandBase {
     private Drivetrain drivetrain;
     private Limelight limelight;
 
-    // Should the command's didFinish() default to true
-    private boolean exitCommand = false;
-    // Number of update cycles where the robot successfully located and adjusted to a target
-    private int iterations;
-
     // Used to weight the horizontal error
     public static double horizontalAdjustmentCoefficient = 0.1; // ! Change this
     // If the absolute value of the horizontal error is less than this threshold, add the minimum absolute adjustment value multiplied by the sign of the horizontal error
@@ -35,10 +30,6 @@ public class AutoTargetAiming extends CommandBase {
     // How much the robot should turn every update cycle if it has not found a target yet
     public static double seekingAdjustment = 0.3; // ! Change this
 
-    // Minimum number of update cycles where the robot successfully located and adjusted to a target
-    // * Is there a way to make this command in the form of Newton's iterative method by keeping track of previous target info?
-    public static int minimumIterations = 8; // ! Change this
-
     public AutoTargetAiming(Drivetrain drivetrain, Limelight limelight) {
         this.drivetrain = drivetrain;
         this.limelight = limelight;
@@ -47,8 +38,6 @@ public class AutoTargetAiming extends CommandBase {
 
     // Called on update cycles where the robot has a target
     private void adjustWithTarget() {
-        this.iterations++;
-
         double targetX = limelight.getXOffset();
         double targetY = limelight.getYOffset();
 
@@ -75,18 +64,11 @@ public class AutoTargetAiming extends CommandBase {
 
     // Called on update cycles where the robot does not have a target
     private void adjustWithoutTarget() {
-        // If it lost or never found the target, then it requires a full series of adjustments
-        this.iterations = 0;
-
         double leftPower = AutoTargetAiming.seekingAdjustment;
         double rightPower = -AutoTargetAiming.seekingAdjustment;
 
         drivetrain.getLeft().getMain().set(leftPower);
         drivetrain.getRight().getMain().set(rightPower);
-    }
-
-    public void exit() {
-        this.exitCommand = true;
     }
 
     @Override
@@ -100,12 +82,12 @@ public class AutoTargetAiming extends CommandBase {
 
     @Override
     public boolean isFinished() {
-        return (this.exitCommand || this.iterations >= AutoTargetAiming.minimumIterations);
+        // what to do for this?
+        return false;
     }
 
     @Override
     public void end(boolean interrupted) {
         drivetrain.setVoltage(0, 0);
-        this.exit();
     }
 }
